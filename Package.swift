5fe66fab8a56ed9cea90fbd01fbd554ae888ef6f
dclife <dclife2009@gmail.com>
2022-04-18 13:13:41 +0200

    update SPM

--- Package.swift
+++ Package.swift
@@ -1,5 +1,21 @@
+// swift-tools-version:5.6
+
 import PackageDescription
 
 let package = Package(
-  name: "Locksmith"
+    name: "Locksmith",
+    platforms: [
+        .macOS(.v10_12),
+        .iOS(.v10),
+        .tvOS(.v10),
+        .watchOS(.v3)],
+    products: [
+        .library(
+            name: "Locksmith",
+            targets: ["Locksmith"])],
+    targets: [
+        .target(
+            name: "Locksmith",
+            path: "Source",
+            exclude: ["Info.plist", "Locksmith.h"])]
 )
